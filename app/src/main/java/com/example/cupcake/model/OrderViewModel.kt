a097800d7795ad61a3428fdb5403a5f54a59c458
TanujKhanna0210 <21bec028@nith.ac.in>
2023-03-17 17:57:01 +0530

    Updating Fragments

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.cupcake.model
 
+import android.Manifest.permission_group.CALENDAR
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class OrderViewModel:ViewModel() {
     private val _quantity = MutableLiveData<Int>(0)
@@ -17,6 +20,8 @@ class OrderViewModel:ViewModel() {
     private val _price = MutableLiveData<Double>(0.0)
     val price : LiveData<Double> = _price
 
+    val dateOptions = getPickUpOptions()
+
     fun setQuantity(numberCupcakes:Int){
         _quantity.value = numberCupcakes
     }
@@ -27,5 +32,20 @@ class OrderViewModel:ViewModel() {
 
     fun setDate(pickUpDate:String){
         _date.value = pickUpDate
+    }
+
+    fun hasNoFlavorSet() : Boolean {
+        return _flavor.value.isNullOrEmpty()
+    }
+
+    private fun getPickUpOptions() : List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calender = Calendar.getInstance()
+        repeat(4){
+            options.add(formatter.format(calender.time))
+            calender.add(Calendar.DATE,1)
+        }
+        return options
     }
 }
